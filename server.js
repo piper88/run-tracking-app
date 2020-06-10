a82905c4ffefbbf711e82336938bc44f5cc3dd22
Sarah DeBey <sarah.debey@gmail.com>
2020-06-10 10:51:24 -0700

    server file and router set up

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const PORT = 3000;
+const router = new Router();
 
 const server = http.createServer((req, res) => {
-
+  router.route(req, res);
 });
 
 server.listen(PORT, () => {
