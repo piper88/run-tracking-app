958622c5f5fdc06190c33506413c3c3b4eb5a51c
Sarah DeBey <sarah.debey@gmail.com>
2020-06-10 14:47:00 -0700

    removing node_modules

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const PORT = 3000;
+const Router = require('./lib/router.js');
 const router = new Router();
+const runRouter = require('./route/run-router.js');
+
+runRouter(router);
+
 
 const server = http.createServer((req, res) => {
   router.route(req, res);
---
+++ test/run-router-tests.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+const assert = require('assert');
+const storage = require('../lib/storage.js')
+
+require('../server.js');
+
+describe('testing run routes', function() {
+  storage.createResourceDirectory();
+
+  describe('testing GET /api/run', function() {
+  describe('with valid run date', function () {
+    it('should return a run', function(done) {
+      request.get('localhost:3000/api/run?date=today')
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.distance).to.equal(2);
+        done();
+      })
+    })
+  })
+  describe('with missing run date', function() {
+    it('should return a 404 not found error', function(done) {
+      request.get('localhost:3000/api/run')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      })
+    })
+  })
+  describe('with invalid run date', function() {
+    it('should return a 404 not found error', function(done) {
+      request.get('localhost:3000/api/run?date=neverp')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      })
+    })
+  })
+})
+
+})
